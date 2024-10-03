e1bfbcaf22c71abb0f2fe946f8138eb8fb944282
kevintsao14 <chtsao@syr.edu>
2024-10-02 21:53:42 -0700

    add github id

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -32,7 +32,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/kevintsao14/kanbas-react-web-app-fa24" className="nav-link">
+        <a id="wd-github" href="https://github.com/kevintsao14/kanbas-react-web-app-fa24" className="nav-link">
           My GitHub
         </a>
       </li>
