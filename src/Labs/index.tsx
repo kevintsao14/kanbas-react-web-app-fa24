ab692d51ca4de49517cbff101911cb413c05d480
kevintsao14 <chtsao@syr.edu>
2024-11-20 23:35:21 -0800

    a5

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -25,7 +25,7 @@ export default function Labs() {
                 <Route path="Lab2" element={<Lab2 />} />
                 <Route path="Lab3/*" element={<Lab3 />} />
                 <Route path="Lab4" element={<Lab4 />} />
-                <Route path="Lab5" element={<Lab5 />} />
+                <Route path="Lab5/*" element={<Lab5 />} />
                 <Route path="Lab6" element={<Lab6 />} />
                 
             </Routes>
