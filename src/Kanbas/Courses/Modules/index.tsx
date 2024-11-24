4886603cf3c21e15fb169312d504e6fc4359d938
kevintsao14 <chtsao@syr.edu>
2024-11-23 22:50:20 -0800

    delte module

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -9,6 +9,8 @@ import { setModules, addModule, editModule, updateModule, deleteModule }
     from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
 import * as coursesClient from "../client";
+import * as modulesClient from "./client";
+
 
 export default function Modules() {
     const { cid } = useParams();
@@ -28,6 +30,11 @@ export default function Modules() {
         const module = await coursesClient.createModuleForCourse(cid, newModule);
         dispatch(addModule(module));
       };
+      const removeModule = async (moduleId: string) => {
+        await modulesClient.deleteModule(moduleId);
+        dispatch(deleteModule(moduleId));
+      };
+    
     
     
     // export default function Modules() {
@@ -82,9 +89,10 @@ export default function Modules() {
                                         defaultValue={module.name} />
                                 )}
                                 <ModuleControlButtons moduleId={module._id}
-                                    deleteModule={(moduleId) => {
-                                        dispatch(deleteModule(moduleId));
-                                    }}
+                                    deleteModule={(moduleId) => removeModule(moduleId)}
+                                    // deleteModule={(moduleId) => {
+                                    //     dispatch(deleteModule(moduleId));
+                                    // }}
                                     editModule={(moduleId) => dispatch(editModule(moduleId))} />
                             </div>
                             {module.lessons && (
