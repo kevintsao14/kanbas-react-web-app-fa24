a66bc228d774e1c13a9dde65b6202fc3ac3bf8f9
kevintsao14 <chtsao@syr.edu>
2024-12-03 23:43:38 -0800

    a6 mongodb

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -25,6 +25,8 @@ export default function AccountNavigation() {
   const {pathname} = useLocation();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
+
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
       <Link id="wd-signin-link"    to="/Kanbas/Account/Signin" 
@@ -37,6 +39,8 @@ export default function AccountNavigation() {
       <Link id="wd-profile-link"  to="/Kanbas/Account/Profile"
       className={`list-group-item border border-0
         ${pathname.includes("Profile") ? "active" : "text-danger"}`}>Profile</Link>
+      {currentUser && currentUser.role === "ADMIN" && (
+       <Link to={`/Kanbas/Account/Users`} className={`list-group-item border-0 ${active("Users")}`}> Users </Link> )}
     </div>
 );}
 
